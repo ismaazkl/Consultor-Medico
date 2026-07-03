e2434d7b9136111997d071e00bc17beb4efa1d9e
ismaaazkl <ismaelcadmen2009@gmail.com>
2026-07-02 19:39:41 -0500

    fix: serve static files from index.php as safety net, simplify Apache config

--- public/index.php
+++ public/index.php
@@ -1,19 +1,40 @@
 <?php
 
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if ($uri !== '/' && !str_contains($uri, 'index.php')) {
+    $file = __DIR__ . $uri;
+    if (is_file($file)) {
+        $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+        $mimes = [
+            'css' => 'text/css',
+            'js'  => 'text/javascript',
+            'png' => 'image/png',
+            'jpg' => 'image/jpeg',
+            'jpeg'=> 'image/jpeg',
+            'svg' => 'image/svg+xml',
+            'ico' => 'image/x-icon',
+            'woff'=> 'font/woff',
+            'woff2'=>'font/woff2',
+        ];
+        if (isset($mimes[$ext])) {
+            header('Content-Type: ' . $mimes[$ext]);
+        }
+        readfile($file);
+        return;
+    }
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
