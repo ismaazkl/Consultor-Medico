42c9376ab3e07a70405a2bc44ba0acd16568f215
ismaaazkl <ismaelcadmen2009@gmail.com>
2026-08-15 15:46:12 -0500

    fix: email link uses mailto to open Gmail app on mobile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -544,7 +544,7 @@
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#0d2d3a" stroke-width="2"><path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z"/></svg>
             Llamar ahora
         </a>
-        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=consultoriomedicovergara@hotmail.com" target="_blank" class="whatsapp-menu-item">
+        <a href="mailto:consultoriomedicovergara@hotmail.com?subject=Consulta%20M%C3%A9dica%20-%20Dr.%20Vergara" class="whatsapp-menu-item">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#0d2d3a" stroke-width="2"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></svg>
             Enviar email
         </a>
